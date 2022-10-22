f76ac5bf67741e0e63937a0f11a9aa1ad1f0b2a6
unknown <nowakpawel99@interia.pl>
2022-10-22 21:55:18 +0200

    Adding config files

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const title = "Our Shop";
+const description = "Our Shop";
+
+const nextSeoConfig = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    site_name: "Our Shop",
+  },
+};
+
+export default nextSeoConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Layout } from "../components/Layout";
+import SEO from "../next-seo.config"
+import { DefaultSeo } from "next-seo";
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO}/>
       <QueryClientProvider client={client}>
         <Component {...pageProps} />
       </QueryClientProvider>
