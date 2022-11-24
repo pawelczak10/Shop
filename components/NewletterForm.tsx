1c96ab242a3b74827c63e939d71cc6bea86fcdb4
unknown <nowakpawel99@interia.pl>
2022-11-24 23:46:45 +0100

    Newletter

---
+++ components/NewletterForm.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { validateCreditCardDate } from "../utils";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const Newsletterform = () => {
+//   const schema = yup
+//     .object({
+//       email: yup.string().email().required(),
+//     })
+//     .required();
+
+//   type FormData = yup.InferType<typeof schema>;
+//   const {
+//     register,
+//     setValue,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<FormData>({
+//     resolver: yupResolver(schema),
+//   });
+
+//   return;
+};
+
+export default Newsletterform;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import type { NextApiHandler } from "next";
+import { responsePathAsArray } from "graphql";
 
-type Data = {
-  name: string
-}
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.setHeader("Allow", "POST").status(405).json({});
+  }
+  const MAILERLITE_API_KEY = process.env.MAILERLITE_API_KEY;
+  const MAILERLITE_GROUP_ID = process.env.MAILERLITE_GROUP_ID;
+  const email = req.body.email;
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  if (typeof email !== "string") {
+    return res.status(400).json({});
+  }
+  if (!MAILERLITE_API_KEY || !MAILERLITE_GROUP_ID) {
+    return res.status(500).json({ error: `Something went wrong!` });
+  }
+  console.log("TEST")
+  console.log(MAILERLITE_API_KEY);
+  console.log(MAILERLITE_GROUP_ID);
+
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "X-MailerLite-ApiKey": MAILERLITE_API_KEY,
+    },
+    body: JSON.stringify({
+      email,
+    }),
+  };
+
+  const response = await fetch(
+    `https://api.mailerlite.com/api/v2/groups/${MAILERLITE_GROUP_ID}/subscribers`,
+    options
+  );
+  console.log(response);
+  if (!response.ok) {
+    return res.status(500).json({
+      error: "Pojawił sie problem",
+    });
+  }
+  return res.status(201).json({});
+  // const json = await response.json();
+};
+
+export default handler;
